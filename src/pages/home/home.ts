8e166b7ab657f84121ae6f4edd09af0f8691bcfa
Nick W <n.waterworth@verizon.net>
2018-06-14 15:34:13 -0500

    Added functionality to the like buton with Logic on converting its values, also changed the json likes object to fit.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,6 +12,43 @@ export class HomePage {
   // TODO: FILTER POSTS BY SELECTED NAV BAR SECTION
 
   constructor(public navCtrl: NavController) {
+
+  }
+  
+  // TODO: MAP BUTTONS TO EFFECT POST OBJECT THEY CLICK
+  Like(post){
+    //Either like or unlike
+    if(post.likes.Hit == "false")
+    {
+      post.likes.Hit = "true";
+      post.likes.Amount =  String (parseInt(post.likes.Amount)+ 1);
+    }
+    else
+    {
+      post.likes.Hit = "false";
+      post.likes.Amount =  String (parseInt(post.likes.Amount)- 1);
+    }
+
+    //Parse the data for Display
+    let likes = post.likes.Amount;
+    if(post.likes.Amount > 999999)
+        post.likes.Display = String(parseInt(likes)).substring(0,2) + "m";
+    else if(post.likes.Amount > 999)
+        post.likes.Display = String(parseInt(likes)).substring(0,2) + "k";
+    else
+      post.likes.Display = String(parseInt(likes));
   }
 
+  Comment(post){
+    //Open interface to take user input
+  }
+
+  Repost(post){
+    //
+
+  }
+
+  Share(post){
+    //Copy link to the post
+  }
 }
