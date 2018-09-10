52f18d56549ff6ea378180c5dc27d39a241ab0b6
WATERWORTHITPC\nwate_000 <n.waterworth@verizon.net>
2018-09-10 17:10:04 -0500

    Added Dynamic Text input box features dependent on feed.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -34,7 +34,7 @@ export class HomePage {
     if(id === "profile")
       this.navCtrl.push(ProfilePage,{ProKey: 0},{animate:false});
   }
-
+  
   //Setting Filter based on input
   filter(type: string){
     this.filt = type;
@@ -43,6 +43,14 @@ export class HomePage {
   //Taking user input to set the feed
   setfeed(type: string){
     this.feed = type;
+	
+	//Set Placeholder values
+	if(this.feed == "public")
+		(<HTMLInputElement>document.getElementById('htextbox')).placeholder = "Say something to the World...";
+	else if(this.feed =="friends")
+		(<HTMLInputElement>document.getElementById('htextbox')).placeholder = "Say something to your Favorites...";
+	else
+		(<HTMLInputElement>document.getElementById('htextbox')).placeholder = "Say something to your Family...";
   }
 
   //Liking a post
